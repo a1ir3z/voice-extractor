54c9befb2ac9bcb00855939e7c2223cb86d36587
alireza <alireza.askari132@gmail.com>
2022-02-01 21:51:38 +0330

    we have to combine segments using dubpy and convert them to mp3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # import things here
 import os
-
-
+from zipfile import ZipFile
+from pydub import AudioSegment
 
 # Make folders
 if not 'voice' in os.listdir():
@@ -31,7 +31,18 @@ for file in os.listdir():
 
 # extract zip file and take the voices out
 
+for power in POWERLIST:
+	with ZipFile(power,'r') as zipObj:
+		for content in zipObj.namelist():
+			if content.endswith('.wav') or content.endswith('.wma') or content.endswith('.m4a') or content.endswith('.mp3') or content.endswith('.MP3') or content.endswith('.WAV') or content.endswith('.wav') 	:
+				zipObj.extract(content,f'{power}-voice')
 
+
+segments_path= os.path.join(f'{power}-voice','ppt','media')
+segments = []
+sort(segments_path)
+for segment in os.listdir(segments_path) :
+	
 
 
 
