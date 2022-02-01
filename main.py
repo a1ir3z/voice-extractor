bbe20a8e596dc47c9d1f2a1bade2297d11405bde
Alireza <alireza.askari132@gmail.com>
2022-02-02 00:17:50 +0330

    updated the main
    just need to add the loop at the end

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-# import things here
 import os
 from zipfile import ZipFile
 from pydub import AudioSegment
-
-# Make folders
-if not 'voice' in os.listdir():
-    os.makedirs('voice')
-if not 'power' in os.listdir():
-    os.makedirs('power')
+import shutil
 
 # get a PPTX and copy it into a folder
 # convert copied pptx into a zip file
 
 POWERLIST =[]
+
 
 for file in os.listdir():
 	if file.endswith('.ppt'):
@@ -28,8 +23,7 @@ for file in os.listdir():
 		POWERLIST.append(file)
 
 
-
-# extract zip file and take the voices out
+		# extract zip file and take the voices out
 
 for power in POWERLIST:
 	with ZipFile(power,'r') as zipObj:
@@ -38,3 +32,24 @@ for power in POWERLIST:
 				zipObj.extract(content,f'{power}-voice')
 
 
+#change directory to voice directory
+source_dir = os.path.join(f'{power}-voice','ppt','media')
+target_dir = 'voice'
+    
+file_names = os.listdir(source_dir)
+    
+for file_name in file_names:
+    shutil.move(os.path.join(source_dir, file_name), target_dir)
+
+os.removedirs(source_dir)
+
+
+#combine 2 voices for test 
+# i have to write a loop to combine all and export them
+voices = os.listdir('voice')
+voice1=  AudioSegment.from_wav('voice\media1.wav')
+voice2=  AudioSegment.from_wav('voice\media2.wav')
+combined = voice1 + voice2
+combined.export ('joined.wav', format="wav")
+
+#convert the file and rename it to the powerpoint name
